ff96b0e2f77690212b6082ee24f1ce54d826d384
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 02:09:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,15 @@
 def badge_maker(array)
-  puts "Hello, my name is #{name}."
+  array.each{|name| puts "Hello, my name is #{name}."}
 end
 
 def batch_badge_creator(array)
+  
+end
+
+def assign_rooms(array)
+  
+end
+
+def printer
   
 end
