7c28fda89f206d24d496a25ac0b18ebfde66abeb
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 02:34:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,9 +12,9 @@ end
 
 def assign_rooms(array)
   counter = 1 
+  counter += 1 
   room_assignements = []
   array.each{|name| room_assignements << "Hello, #{name}! You'll be assigned to room #{counter}!"}
-  counter += 1 
   room_assignements
 end
 
